507385ec0ab1d2b444b741854591577abde60ec1
Hector Velasco <hvelasco0@ucol.mx>
2022-10-12 16:40:49 -0500

    feat(HomeProfeScreen)

--- frontend/src/pages/HomeProfe.js
+++ frontend/src/pages/HomeProfe.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TopBar from "../components/TopBar";
+import ClaseCard from "../components/ClaseCard";
 
 const HomeProfe = () => {
   return (
     <div>
       <TopBar />
 
-      <div className="flex justify-around pt-10">
+      <div className="flex justify-around pt-10 pb-10">
         {/* Pequeña card del user  */}
         <div className="flex items-center shadow px-5  rounded-xl bg-white hover:border-2 border-indigo-600">
           <img
@@ -21,7 +22,7 @@ const HomeProfe = () => {
 
         <h1 className="text-black font-black text-6xl">Mis Clases</h1>
 
-        <div className="flex items-center shadow px-5  rounded-xl bg-white hover:border-2 border-indigo-600">
+        <div className="flex items-center shadow px-5   rounded-xl bg-white hover:border-2 border-indigo-600">
           <div className="text-md">
             <p className="text-gray-900 leading-none">Crear nueva clase</p>
           </div>
@@ -29,15 +30,21 @@ const HomeProfe = () => {
       </div>
 
       {/*Clases */}
+      <div>
+        <div className="flex justify-around pb-10 pt-10">
+          <ClaseCard titulo={"Ingles"} grado={1} grupo={"A"} />
+          <ClaseCard titulo={"Matematicas"} grado={1} grupo={"D"} />
+          <ClaseCard titulo={"Historia"} grado={2} grupo={"B"} />
+        </div>
+
+        <div className="flex justify-around">
+          <ClaseCard titulo={"Ciencias"} grado={3} grupo={"A"} />
+          <ClaseCard titulo={"Geografia"} grado={3} grupo={"C"} />
+          <ClaseCard titulo={"Español"} grado={5} grupo={"D"} />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default HomeProfe;
-/*<div className="flex items-center bg-gray-200">
-      <img className="w-10 h-10 rounded-full mr-4" src="https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fmag.elcomercio.pe%2Ffama%2Fel-chavo-del-8-el-verdadero-origen-del-profesor-jirafales-ruben-aguirre-los-supergenios-de-la-mesa-cuadrada-series-mexico-mx-nnda-nnlt-noticia%2F&psig=AOvVaw2im8ipEWVBDZC-A349uX3L&ust=1665463021315000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCODZr7Hr1PoCFQAAAAAdAAAAABAO" alt="Avatar of Jonathan Reinink">
-      <div className="text-sm">
-        <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-        <p className="text-gray-600">Aug 18</p>
-        </div>
-      </div>*/
