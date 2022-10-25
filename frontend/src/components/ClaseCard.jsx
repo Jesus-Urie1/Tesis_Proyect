8e0bd2f1d1615a2f803845a1fcd9290e12245e5c
Hector Velasco <hvelasco0@ucol.mx>
2022-10-25 01:51:50 -0500

    syle(HomeProfe): Nuevo diseño

--- frontend/src/components/ClaseCard.jsx
+++ frontend/src/components/ClaseCard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { HiDotsVertical } from "react-icons/hi";
 
 const ClaseCard = ({ titulo, grado, grupo }) => {
   return (
-    <div className="bg-white shadow-lg rounded-xl border-2 hover:border-2 hover:border-indigo-600 grid justify-items-center py-5 px-10">
-      <div className="text-black/500 font-semibold text-2xl pb-20">
-        {titulo}
+    <div className="bg-white shadow-lg rounded-xl border-2 hover:border-2 hover:border-indigo-600 grid justify-items-center py-5 pl-5 pr-5">
+      <div className="flex justify-between  container">
+        <div className="text-black/500 font-semibold text-2xl pb-20 ">
+          {titulo}
+        </div>
+        <div className="rounded-full hover:bg-indigo-100 p-2 h-9">
+          <HiDotsVertical color="black" size={20} />
+        </div>
       </div>
+
       <div className="flex justify-around ">
         <h6 className="pr-20 flex italic">
           Grado: <div className="pl-2 font-medium"> {grado}</div>
