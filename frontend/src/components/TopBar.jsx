d98a9f7420d65108bb53881a72284384020b2a05
Hector Velasco <hvelasco0@ucol.mx>
2022-11-01 17:55:36 -0600

    feat(Responsive design Homepage)

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -8,38 +8,29 @@ const TopBar = () => {
     <nav className="flex items-center justify-between flex-wrap bg-indigo-700 p-3">
       <div className="flex">
         <button
-          className="flex items-center p-2  rounded-full hover:bg-indigo-600 mr-2 ml-2"
+          className="flex items-center p-2  rounded-full hover:bg-indigo-600 mr-2"
           data-collapse-toggle="navbar-default"
         >
           <HiMenu color="white" size={26} />
         </button>
 
         <div className="flex items-center flex-shrink-0 text-white">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
           <span className="font-semibold text-xl tracking-tight">
-            Plataforma EduCol
+            Plataforma Educativa
           </span>
         </div>
       </div>
+
       <div className="flex items-center">
-        <button className="p-1 mx-5 rounded-full hover:bg-indigo-600">
+        <button className="p-1 rounded-full hover:bg-indigo-600 mr-2">
           <HiPlus color="white" size={30} />
         </button>
         <button className="rounded-full hover:bg-indigo-600 p-1">
-
-        <img
-          src="https://img2.gratispng.com/20180613/egk/kisspng-professor-stock-photography-5b218ab481d817.8727019215289248525319.jpg"
-          className="w-9 h-9 rounded-full"
+          <img
+            src="https://img2.gratispng.com/20180613/egk/kisspng-professor-stock-photography-5b218ab481d817.8727019215289248525319.jpg"
+            className="w-9 h-9 rounded-full"
           />
-          </button>
+        </button>
       </div>
     </nav>
   );
--- frontend/src/pages/HomeProfe.jsx
+++ frontend/src/pages/HomeProfe.jsx
@@ -4,28 +4,21 @@ import ClaseCard from "../components/ClaseCard";
 
 const HomeProfe = () => {
   return (
-    
     <div>
       <TopBar />
 
-      <div className="flex justify-around pt-10 pb-10">
-        
+      <div className="flex justify-around pt-10">
         <h1 className="text-black font-black text-6xl">Mis Clases</h1>
       </div>
 
-      {/*Clases */}
-      <div>
-        <div className="flex justify-around pb-10 pt-10">
-          <ClaseCard titulo={"Ingles"} grado={1} grupo={"A"} />
-          <ClaseCard titulo={"Matematicas"} grado={1} grupo={"D"} />
-          <ClaseCard titulo={"Historia"} grado={2} grupo={"B"} />
-        </div>
+      <div className="flex flex-wrap justify-center xl:justify-start mt-10 ml-5">
+        <ClaseCard titulo={"Ingles"} grado={1} grupo={"A"} />
+        <ClaseCard titulo={"Matematicas"} grado={1} grupo={"D"} />
+        <ClaseCard titulo={"Historia"} grado={2} grupo={"B"} />
 
-        <div className="flex justify-around">
-          <ClaseCard titulo={"Ciencias"} grado={3} grupo={"A"} />
-          <ClaseCard titulo={"Geografia"} grado={3} grupo={"C"} />
-          <ClaseCard titulo={"Español"} grado={5} grupo={"D"} />
-        </div>
+        <ClaseCard titulo={"Ciencias"} grado={3} grupo={"A"} />
+        <ClaseCard titulo={"Geografia"} grado={3} grupo={"C"} />
+        <ClaseCard titulo={"Español"} grado={5} grupo={"D"} />
       </div>
     </div>
   );
