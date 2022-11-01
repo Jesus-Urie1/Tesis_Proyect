a2636e45c0968923f964386552ddb5096423b8e8
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-10-31 23:12:37 -0600

    Model Maestro

---
+++ backend/models/Maestro.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import generarId from "../helpers/generarId.js";
+
+//Schema de Estudiante
+const maestroSchema = mongoose.Schema({
+  nombre: {
+    type: String, //Tipo de dato
+    required: true, //Validacion del servidor
+    trim: true, //Eliminamos los espacion en blanco
+  },
+  apellidos: {
+    type: String, //Tipo de dato
+    required: true, //Validacion del servidor
+    trim: true, //Eliminamos los espacion en blanco
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, //Garantizamos que usamos un email por cuenta
+    trim: true,
+  },
+  token: {
+    type: String,
+    default: generarId(),
+  },
+  tipoCuenta: {
+    type: String, //Tipo de dato
+    required: true, //Validacion del servidor
+    trim: true, //Eliminamos los espacion en blanco
+  },
+  confirmado: {
+    //Para confirmar cuenta
+    type: Boolean,
+    default: false,
+  },
+});
+
+//Hasheando el password
+maestroSchema.pre("save", async function (next) {
+  //Un password que ya esta hasheado, no lo vuelva a hashear
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10); //Generar el Salt
+  this.password = await bcrypt.hash(this.password, salt); //Hasheando el password
+});
+
+//Comprobando password
+maestroSchema.methods.comprobarPassword = async function (passwordFormulario) {
+  return await bcrypt.compare(passwordFormulario, this.password);
+};
+
+const Maestro = mongoose.model("Maestro", maestroSchema); //Registrar el modelo
+
+export default Maestro;
