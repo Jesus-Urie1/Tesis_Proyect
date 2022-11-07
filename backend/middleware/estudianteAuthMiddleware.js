756a8af66daef50d2019f746d023a5096a1e533a
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-11-07 16:57:52 -0600

    estudianteAuthMiddleware.js

---
+++ backend/middleware/estudianteAuthMiddleware.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import Estudiante from "../models/Estudiante.js";
+
+const estudianteAuth = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //Buscando mediante el ID al estudiante
+      req.usuario = await Estudiante.findById(decoded.id).select(
+        "-password -token -confirmado"
+      );
+
+      if (!req.usuario) {
+        const e = new Error(" Token no valido ");
+        res.status(403).json({ msg: e.message });
+      }
+      return next();
+    } catch (error) {
+      const e = new Error(" Token no valido ");
+      res.status(403).json({ msg: e.message });
+    }
+  }
+
+  //No se mando ningun token
+  if (!token) {
+    const error = new Error("Token no valido o inexistente");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  next();
+};
+
+export default estudianteAuth;
