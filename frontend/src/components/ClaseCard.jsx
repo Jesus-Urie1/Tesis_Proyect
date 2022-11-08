b4dc0ea0b0fe6baf7973daaab684eed8715ea70d
Hector Velasco <hvelasco0@ucol.mx>
2022-11-08 15:27:38 -0600

    responsive design fixed

--- frontend/src/components/ClaseCard.jsx
+++ frontend/src/components/ClaseCard.jsx
@@ -3,7 +3,7 @@ import { HiDotsVertical } from "react-icons/hi";
 
 const ClaseCard = ({ titulo, grado, grupo }) => {
   return (
-    <div className="bg-white shadow-lg rounded-xl border-2 hover:border-2 hover:border-indigo-600 justify-items-center p-7 w-80 mr-5 mb-5">
+    <div className="bg-white shadow-lg rounded-xl border-2 hover:border-2 hover:border-indigo-600 justify-items-center p-7 w-80 mr-3 mb-5">
       <div className="flex justify-between  container ">
         <div className="text-black/500 font-semibold text-2xl">{titulo}</div>
         <div className="rounded-full hover:bg-indigo-100 p-2">
