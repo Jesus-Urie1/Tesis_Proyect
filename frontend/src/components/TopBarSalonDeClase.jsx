c7297e62f5c0114639940a4954035cd4a119d9b1
Jesus-Urie1 <jesusurielvp@gmail.com>
2022-11-08 15:10:41 -0600

    TopBarSalonDeClase.jsx

---
+++ frontend/src/components/TopBarSalonDeClase.jsx
@@ -0,0 +1,34 @@
+import useAuth from "../hooks/useAuth";
+import { HiPlus, HiMenu } from "react-icons/hi";
+
+const TopBarSalonDeClase = ({ setShowClassModal }) => {
+  const { cerrarSesion } = useAuth();
+
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-indigo-700 p-3">
+      <div className="flex">
+        <button
+          className="flex items-center p-2  rounded-full hover:bg-indigo-600 mr-2"
+          data-collapse-toggle="navbar-default"
+        >
+          <HiMenu color="white" size={26} />
+        </button>
+
+        <div className="flex items-center flex-shrink-0 text-white">
+          <span className="font-semibold text-xl tracking-tight">Español</span>
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <button className="rounded-full hover:bg-indigo-600 p-1">
+          <img
+            src="https://img2.gratispng.com/20180613/egk/kisspng-professor-stock-photography-5b218ab481d817.8727019215289248525319.jpg"
+            className="w-9 h-9 rounded-full"
+          />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default TopBarSalonDeClase;
