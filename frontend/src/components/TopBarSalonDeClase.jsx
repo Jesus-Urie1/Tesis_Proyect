df25a8296fe9830007d0e785f8311a4acce98c59
Hector Velasco <hvelasco0@ucol.mx>
2022-11-10 23:04:40 -0600

    feat(topBarMaestrosUpdate)

--- frontend/src/components/TopBarSalonDeClase.jsx
+++ frontend/src/components/TopBarSalonDeClase.jsx
@@ -5,28 +5,33 @@ const TopBarSalonDeClase = ({ setShowClassModal }) => {
   const { cerrarSesion } = useAuth();
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-indigo-700 p-3">
-      <div className="flex">
-        <button
-          className="flex items-center p-2  rounded-full hover:bg-indigo-600 mr-2"
-          data-collapse-toggle="navbar-default"
-        >
-          <HiMenu color="white" size={26} />
-        </button>
+    <nav className=" bg-indigo-700 p-3">
+      <div className="flex items-center justify-between flex-wrap">
+        <div className="flex">
+          <button
+            className="flex items-center p-2  rounded-full hover:bg-indigo-600 mr-2"
+            data-collapse-toggle="navbar-default"
+          >
+            <HiMenu color="white" size={26} />
+          </button>
 
-        <div className="flex items-center flex-shrink-0 text-white">
-          <span className="font-semibold text-xl tracking-tight">Español</span>
+          <div className="flex items-center flex-shrink-0 text-white">
+            <span className="font-semibold text-xl tracking-tight">
+              Español
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <button className="rounded-full hover:bg-indigo-600 p-1">
+            <img
+              src="https://img2.gratispng.com/20180613/egk/kisspng-professor-stock-photography-5b218ab481d817.8727019215289248525319.jpg"
+              className="w-9 h-9 rounded-full"
+            />
+          </button>
         </div>
       </div>
-
-      <div className="flex items-center">
-        <button className="rounded-full hover:bg-indigo-600 p-1">
-          <img
-            src="https://img2.gratispng.com/20180613/egk/kisspng-professor-stock-photography-5b218ab481d817.8727019215289248525319.jpg"
-            className="w-9 h-9 rounded-full"
-          />
-        </button>
-      </div>
+      <div>asd</div>
     </nav>
   );
 };
