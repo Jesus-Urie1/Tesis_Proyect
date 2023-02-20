b3cc9cab81081fca2a06e76fc1e402705e350021
Jesus-Urie1 <jesusurielvp@gmail.com>
2023-02-19 18:39:06 -0600

    Frontend remove

--- frontend/src/components/ClaseCard.jsx
+++
@@ -1,9 +0,0 @@
-const ClaseCard = ({ grado, grupo }) => {
-  return (
-    <div className="text-green-700 hover:text-green-600 bg-white shadow-lg rounded-xl border-2 hover:border-2 hover:border-green-600 m-10  w-40 h-40 mr-3 mb-5 flex justify-center items-center ">
-      <div className="pl-2 font-medium text-8xl "> {grupo}</div>
-    </div>
-  );
-};
-
-export default ClaseCard;
